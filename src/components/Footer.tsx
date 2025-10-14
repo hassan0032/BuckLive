5f2259bc71606edbd9858cffa98079faa2687553
Sharif Ezzat <goodsharif@gmail.com>
2025-10-14 15:44:41 -0700

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -21,7 +21,7 @@ export const Footer: React.FC = () => {
           <div className="flex items-center space-x-3">
             <div>
               <img src="/live-logo-solo.png" alt="Logo" className="h-10 w-auto" />
-              <span className="text-sm">
+              <span className="text-sm text-grey">
                 © {currentYear} Buck Institute. All rights reserved.
               </span>
             </div>
